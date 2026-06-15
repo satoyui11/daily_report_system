feb7d4544883c3ef5960f8d186fc6d450efb336c
satoyui11 <yui-sato@fissc.co.jp>
2026-06-15 14:33:11 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -11,59 +11,62 @@ import constants.MessageConst;
 import constants.PropertyConst;
 import services.EmployeeService;
 
-public class AuthAction extends ActionBase{
+public class AuthAction extends ActionBase {
 
-    
     private EmployeeService service;
-    
+
     @Override
-    public void process() throws ServletException,IOException{
-        service=new EmployeeService();
-        
+    public void process() throws ServletException, IOException {
+        service = new EmployeeService();
+
         invoke();
         service.close();
     }
-    
-    public void showLogin() throws ServletException,IOException{
-        putRequestScope(AttributeConst.TOKEN,getTokenId());
-        
-        
-        String flush=getSessionScope(AttributeConst.FLUSH);
-        if(flush!=null) {
+
+    public void showLogin() throws ServletException, IOException {
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
             putRequestScope(AttributeConst.FLUSH, flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
-        
+
         forward(ForwardConst.FW_LOGIN);
     }
-    
-    public void login() throws ServletException,IOException{
-        
-        String code=getRequestParam(AttributeConst.EMP_CODE);
-        String plainPass=getRequestParam(AttributeConst.EMP_PASS);
-        String pepper=getContextScope(PropertyConst.PEPPER);
-        
-        Boolean isValidEmployee=service.validateLogin(code, plainPass, pepper);
-        
-        if(isValidEmployee) {
-            if(checkToken()) {
-                EmployeeView ev=service.findOne(code,plainPass,pepper);
-                putSessionScope(AttributeConst.LOGIN_EMP,ev);
+
+    public void login() throws ServletException, IOException {
+
+        String code = getRequestParam(AttributeConst.EMP_CODE);
+        String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+        if (isValidEmployee) {
+            if (checkToken()) {
+                EmployeeView ev = service.findOne(code, plainPass, pepper);
+                putSessionScope(AttributeConst.LOGIN_EMP, ev);
                 putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
                 redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
-                
+
             }
-            
-        }else {
-            
+
+        } else {
+
             putRequestScope(AttributeConst.TOKEN, getTokenId());
             putRequestScope(AttributeConst.LOGIN_ERR, true);
             putRequestScope(AttributeConst.EMP_CODE, code);
-            
+
             forward(ForwardConst.FW_LOGIN);
         }
-                
     }
-    
-    
+
+    public void logout() throws ServletException, IOException {
+
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+    }
+
 }
