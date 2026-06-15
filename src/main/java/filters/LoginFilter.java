41a3c8b4170f2fbeab64741525a81be56fe5504a
satoyui11 <yui-sato@fissc.co.jp>
2026-06-15 15:27:23 +0900

    ログインフィルタの作成

---
+++ src/main/java/filters/LoginFilter.java
@@ -0,0 +1,106 @@
+package filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import actions.views.EmployeeView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/*")
+public class LoginFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public LoginFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String contextPath=((HttpServletRequest)request).getContextPath();
+        String servletPath=((HttpServletRequest)request).getServletPath();
+        
+        if(servletPath.matches("/css.*")) {
+            chain.doFilter(request, response);
+        }else {
+            HttpSession session=((HttpServletRequest)request).getSession();
+        
+        
+        String action=request.getParameter(ForwardConst.ACT.getValue());
+        String command=request.getParameter(ForwardConst.CMD.getValue());
+        
+        EmployeeView ev=(EmployeeView) session.getAttribute(AttributeConst.LOGIN_EMP.getValue());
+        
+        if(ev == null) {
+            if(!(ForwardConst.ACT_AUTH.getValue().equals(action)
+                    && (ForwardConst.CMD_SHOW_LOGIN.getValue().equals(command)
+                             || ForwardConst.CMD_LOGIN.getValue().equals(command)))) {
+                
+                ((HttpServletResponse)response).sendRedirect(
+                        contextPath
+                        +"?action="+ForwardConst.ACT_AUTH.getValue()
+                        +"&command="+ForwardConst.CMD_SHOW_LOGIN.getValue());
+                return;
+            }
+        }else {
+            if (ForwardConst.ACT_AUTH.getValue().equals(action)) {
+                if(ForwardConst.CMD_SHOW_LOGIN.getValue().equals(command)) {
+                    ((HttpServletResponse) response).sendRedirect(
+                            contextPath
+                            +"?action="+ForwardConst.ACT_TOP.getValue()
+                            +"&command="+ForwardConst.CMD_INDEX.getValue());
+                    return;
+                    
+                }else if (ForwardConst.CMD_LOGOUT.getValue().equals(command)) {
+                    
+                }else {
+                    String forward=String.format("/WEB-INF/views/%s.jsp", "error/unknown");
+                    RequestDispatcher dispatcher=request.getRequestDispatcher(forward);
+                    dispatcher.forward(request, response);
+                    
+                    return;
+                }
+            }
+        }
+        
+
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
